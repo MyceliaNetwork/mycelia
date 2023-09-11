c25ab28ebe88cc14d8b2949c86898e376ac2372c
Hazel <79116616+SuddenlyHazel@users.noreply.github.com>
2023-09-11 05:01:23 +0000

    logging

--- development_server/src/main.rs
+++ development_server/src/main.rs
@@ -12,19 +12,16 @@ use hyper::{
     body::HttpBody, server::conn::AddrStream, service::make_service_fn, Body, Request, Response,
     Server,
 };
+use log::{warn, info};
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+use wasmtime_components::runtime::StoreProducer;
 
-use tower::{util::BoxService, BoxError, Service, ServiceExt};
-
-async fn handle_function_request(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("hello world".into()))
-}
-
-async fn handle_rpc_request(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("hello world".into()))
-}
+use tower::{
+    util::{BoxCloneService, BoxService},
+    BoxError, Service, ServiceExt,
+};
 
 type ServiceCommandSink = tokio::sync::mpsc::Sender<()>;
 type ServiceCommandSource = tokio::sync::mpsc::Receiver<()>;
@@ -66,8 +63,8 @@ async fn map_request(req: Request<Body>) -> HttpRequest {
         })
         .collect();
     let uri = req.uri().to_string();
-    // body::to_bytes is unsafe to use alone, don't do this in prod.
 
+    // body::to_bytes is unsafe to use alone, don't do this in prod.
     let body = hyper::body::to_bytes(req.boxed()).await;
     let body = body.map(|v| v.to_vec()).unwrap_or(vec![]);
 
@@ -91,78 +88,62 @@ fn map_response(response: HttpResponse) -> Response<Body> {
     builder.body(body).expect("Failed to create a response")
 }
 
-struct HttpService {
-    component_service: FunctionComponentService,
+// this is clearly abusing `map_request`
+// tower should have a way to "place a service"
+// in front of another one
+fn map_http_request(req: Request<Body>) -> HttpRequest {
+    tokio::task::block_in_place(|| {
+        let rt = tokio::runtime::Handle::current();
+        rt.block_on(map_request(req))
+    })
 }
 
-impl Service<Request<Body>> for HttpService {
-    type Response = Response<Body>;
+fn map_component_response(service: FunctionComponentService) -> HttpFunctionComponent {
+    let binding = service
+        .map_request(map_http_request)
+        .map_response(|resp| map_response(resp));
+    binding.boxed()
+}
 
-    type Error = BoxError;
+type HttpFunctionComponent = BoxService<Request<Body>, Response<Body>, BoxError>;
 
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+type HttpFunctionComponentMaker = BoxCloneService<(), HttpFunctionComponent, BoxError>;
 
-    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        todo!()
-    }
+fn build_http_function_component_svc() -> HttpFunctionComponentMaker {
+    let store_producer = wasmtime_components::runtime::make_store_producer();
+    let base_component = function_service::service::empty_base_function_component();
 
-    fn call(&mut self, _request: Request<Body>) -> Self::Future {
-        todo!()
-    }
+    new_function_service_maker(base_component, store_producer)
+        .map_response(|v| map_component_response(v))
+        .boxed_clone()
 }
 
 async fn start_development_server(
     mut command_stream: ServiceCommandSource,
     socket_addr: SocketAddr,
 ) {
-    type HttpFunctionComponent = BoxService<Request<Body>, Response<Body>, BoxError>;
-
-    let store_producer = wasmtime_components::runtime::make_store_producer();
-    let base_component = function_service::service::empty_base_function_component();
-
-    let function_service_maker = new_function_service_maker(base_component, store_producer);
-
-    // this is clearly abusing `map_request`
-    // tower should have a way to "place a service"
-    // in front of another one
-    fn resolve_map_future(req: Request<Body>) -> HttpRequest {
-        tokio::task::block_in_place(|| {
-            let rt = tokio::runtime::Handle::current();
-            rt.block_on(map_request(req))
-        })
-    }
-
-    fn map_function_service(service: FunctionComponentService) -> HttpFunctionComponent {
-        let binding = service
-            .map_request(resolve_map_future)
-            .map_response(|resp| map_response(resp));
-        binding.boxed()
-    }
-
-    let function_service_maker = function_service_maker
-        .map_response(|v| map_function_service(v))
-        .boxed_clone();
-
     let (shutdown_tx, shutdown_rx) = tokio::sync::oneshot::channel();
 
-    let mut function_service_maker = function_service_maker.boxed_clone();
+    let mut function_service_maker = build_http_function_component_svc();
 
     let component_host_server = Server::bind(&socket_addr)
-        .serve(make_service_fn(move |_v: &AddrStream| {
+        .serve(make_service_fn(move |v: &AddrStream| {
+            info!("http connection {:#?}", v);
             function_service_maker.call(())
         }))
         .with_graceful_shutdown(async move {
             let _ = shutdown_rx.await;
         });
 
-    let _server_handle = tokio::spawn(async move {
+    let _ = tokio::spawn(async move {
         while let Some(_command) = command_stream.recv().await {
             // Handle Command
         }
         let _ = shutdown_tx.send(());
     });
-    component_host_server.await;
-    println!("component_host_server returned");
+    let _ = component_host_server.await;
+
+    warn!("component_host_server returned");
 }
 
 pub(crate) mod protos {
@@ -208,33 +189,44 @@ mod cmd {
     #[command(author, version, about, long_about = None)]
     pub struct Args {
         /// path to a function component
-        #[arg(short, long)]
-        function_component: Option<String>,
+        #[arg(long)]
+        pub function_component: Option<String>,
+
+        /// port rpc server should bind to
+        #[arg(long)]
+        pub rpc_port: Option<u16>,
+
+        /// port http server should bind to
+        #[arg(long)]
+        pub http_port: Option<u16>,
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let _args = crate::cmd::Args::parse();
+    env_logger::init();
+    info!("starting up");
 
-    let rpc_host_addr = SocketAddr::from(([127, 0, 0, 1], 50051));
-    let http_host_addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let args = crate::cmd::Args::parse();
+
+    let rpc_host_addr = SocketAddr::from(([127, 0, 0, 1], args.rpc_port.unwrap_or(50051)));
+    let http_host_addr = SocketAddr::from(([127, 0, 0, 1], args.http_port.unwrap_or(3001)));
 
     // Command Sink / Source
     let (command_sink, command_source) = tokio::sync::mpsc::channel(10);
 
-    let rpc_server_future = start_rpc_server(command_sink, rpc_host_addr);
-    let http_server_future = start_development_server(command_source, http_host_addr);
+    let rpc_server = start_rpc_server(command_sink, rpc_host_addr);
+    let http_server = start_development_server(command_source, http_host_addr);
 
-    let rpc_server_task_handle = tokio::spawn(rpc_server_future);
-    let http_service_task_handle = tokio::spawn(http_server_future);
+    let rpc_server = tokio::spawn(rpc_server);
+    let http_server = tokio::spawn(http_server);
 
     tokio::select! {
-        _ = rpc_server_task_handle => {
-            println!("rpc server task completed");
+        _ = rpc_server => {
+            warn!("rpc server task completed");
         }
-        _ = http_service_task_handle => {
-            println!("http server task completed");
+        _ = http_server => {
+            warn!("http server task completed");
         }
     }
 }
