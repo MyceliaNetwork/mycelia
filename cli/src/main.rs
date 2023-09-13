f1c7cc99e3f4615cbe1bc3625d5d4a436b4c0b7a
Daniël Terwiel <daniel@terwiel.io>
2023-09-13 15:38:14 +0200

    [fix] starting develpment_server w/o clap

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,19 +1,33 @@
-use std::{fs::File, io::Error};
+use std::{
+    env,
+    io::Error,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-pub struct Args {
-    /// Wasm file to invoke
-    #[arg(short, long)]
-    path: String,
-}
+// #[derive(Parser, Debug)]
+// #[command(author, version, about, long_about = None)]
+// pub struct Args {
+//     #[arg(short, long)]
+//     path: String,
+// }
 
 fn main() -> Result<(), Error> {
-    let args = Args::parse();
+    // let args = Args::parse();
 
-    let _file = File::open(args.path)?;
+    let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
+    let status = Command::new(cargo)
+        .current_dir(project_root())
+        .args(&["run", "--package=development_server"])
+        .status()?;
 
     Ok(())
+}
+
+fn project_root() -> PathBuf {
+    Path::new(&env!("CARGO_MANIFEST_DIR"))
+        .ancestors()
+        .nth(1)
+        .unwrap()
+        .to_path_buf()
 }
