5c4974b9c78c02fd2c90628e6085959a11940737
Daniël Terwiel <daniel@terwiel.io>
2023-09-15 00:52:43 +0200

    fix: `cargo run stop` bugged after build

--- cli/build.rs
+++ cli/build.rs
@@ -6,10 +6,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // See https://github.com/hyperium/tonic/blob/master/examples/build.rs
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("cli_descriptor.bin"))
-        .compile(&["proto/cli.proto"], &["proto"])
+        .file_descriptor_set_path(out_dir.join("development_descriptor.bin"))
+        .compile(&["proto/development.proto"], &["proto"])
         .unwrap();
 
-    tonic_build::compile_protos("proto/cli.proto")?;
+    tonic_build::compile_protos("proto/development.proto")?;
     Ok(())
 }
