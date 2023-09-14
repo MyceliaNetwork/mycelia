315ca3f8b1824230d35dd8ffa6c40b7257aa4f45
Daniël Terwiel <daniel@terwiel.io>
2023-09-14 16:06:03 +0200

    [feat]: impl tonic client

---
+++ cli/build.rs
@@ -0,0 +1,15 @@
+// FIXME: this is a copy of /development_server/build.rs
+
+use std::{env, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // See https://github.com/hyperium/tonic/blob/master/examples/build.rs
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("development_descriptor.bin"))
+        .compile(&["proto/development.proto"], &["proto"])
+        .unwrap();
+
+    tonic_build::compile_protos("proto/development.proto")?;
+    Ok(())
+}
