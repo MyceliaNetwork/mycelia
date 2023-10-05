2e6ea6edc0f6f8b32a308477d166d39d30fea2db
Daniël Terwiel <daniel@terwiel.io>
2023-10-05 19:10:59 +0200

    [chore]: cargo run release works

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -311,10 +311,11 @@ fn build_component(guest: &Guest) -> Result<(), BuildError> {
 
 async fn release() -> Result<(), ReleaseError> {
     if let Err(error) = try_release().await {
-        error!("{error}");
+        eprintln!("{:#?}", error);
         return Err(error);
     }
 
+    println!("hoax");
     return Ok(());
 }
 
@@ -334,18 +335,24 @@ async fn try_release() -> Result<(), ReleaseError> {
         return Err(ReleaseError::MissingVersionVal);
     }
 
-    let version_comparison = compare(version_current, version_arg_val.clone().unwrap());
+    println!(
+        "🪵 [main.rs:337]~ token ~ \x1b[0;32mversion_current\x1b[0m = {}",
+        version_current
+    );
+    let version_comparison = compare(version_arg_val.clone().unwrap(), version_current);
     if version_comparison.is_err() {
         return Err(ReleaseError::VersionComparisonError);
     }
     match version_comparison.unwrap() {
         Cmp::Lt => {
+            println!("1");
             return Err(ReleaseError::VersionLowerThanCurrent {
                 version_input: version_arg_val.clone().unwrap(),
                 version_current: version_current.to_string(),
             });
         }
         Cmp::Eq => {
+            println!("2");
             return Err(ReleaseError::VersionEqualToCurrent {
                 version_input: version_arg_val.clone().unwrap(),
                 version_current: version_current.to_string(),
@@ -354,6 +361,7 @@ async fn try_release() -> Result<(), ReleaseError> {
         _ => {}
     }
 
+    println!("test");
     build_workspace_release().await?;
     rustwrap(version_arg_val.unwrap()).await?;
 
